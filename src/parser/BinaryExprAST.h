a7e1854f260ca6b09fb6f0e290cd52beda78b03f
euclideanrn <13493334+euclideanrn@users.noreply.github.com>
2023-01-08 18:47:05 -0500

    define ast types rename my name in comment

---
+++ src/parser/BinaryExprAST.h
@@ -0,0 +1,18 @@
+//
+// Created by FANYI ZHAO on 1/8/23.
+//
+#pragma once
+
+#include <string>
+#include "ExprAST.h"
+
+using std::unique_ptr;
+
+class BinaryExprAST : public ExprAST {
+private:
+    char Op;
+    unique_ptr<ExprAST> LHS, RHS;
+
+public:
+    BinaryExprAST(char op, unique_ptr<ExprAST> LHS, unique_ptr<ExprAST> RHS): Op(op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
---
+++ src/parser/FunctionAST.h
@@ -0,0 +1,20 @@
+//
+// Created by FANYI ZHAO on 1/8/23.
+//
+#pragma once
+
+#include <string>
+#include <vector>
+#include "PrototypeAST.h"
+#include "ExprAST.h"
+
+using std::unique_ptr;
+
+class FunctionAST {
+private:
+    std::unique_ptr<PrototypeAST> Proto;
+    std::unique_ptr<ExprAST> Body;
+
+public:
+    FunctionAST(unique_ptr<PrototypeAST> Proto, unique_ptr<ExprAST> Body) : Proto(std::move(Proto)), Body(std::move(Body)) {}
+};
